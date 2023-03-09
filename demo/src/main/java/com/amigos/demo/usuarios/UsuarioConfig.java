dc48cebe875bbfe9cb247a0dec20b78f668b6808
Javier Trevijano <javier.trevijano@hotmail.com>
2023-03-09 13:08:01 +0100

    Etiqueta Bean en UsuarioConfig

--- demo/src/main/java/com/amigos/demo/usuarios/UsuarioConfig.java
+++ demo/src/main/java/com/amigos/demo/usuarios/UsuarioConfig.java
@@ -1,16 +1,30 @@
 package com.amigos.demo.usuarios;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class UsuarioConfig {
+    @Bean
     CommandLineRunner commandLineRunner(UsuarioRepository repository){
         return args -> {
-            new Usuario(1L,
+            Usuario antonio = new Usuario(1L,
                     "Antonio",
                     "802020",
                     "toni@gmail.com");
+            Usuario ana = new Usuario(2L,
+                    "Ana",
+                    "305958",
+                    "ana@gmail.com");
+            Usuario alex = new Usuario(3L,
+                    "Alex",
+                    "345958",
+                    "alexander@gmail.com");
+
+            repository.saveAll(List.of(ana,alex));
         };
     }
 }
